1d2d703b984a9c97c0a232cffc5593a6bec613cb
clinton <dxl3730@gmail.com>
2025-01-03 22:08:45 +0100

    sixth commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const words = ["Hello", "Welcome", "Javascript", "Animation", "Coding"];
+const textElement = document.getElementById("text");
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  const currentWord = words[wordIndex];
+  if (isDeleting) {
+    charIndex--;
+    textElement.textContent = currentWord.substring(0, charIndex);
+  } else {
+    charIndex++;
+    textElement.textContent = currentWord.substring(0, charIndex);
+  }
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    setTimeout(() => {
+      isDeleting = true;
+    }, 1000); // Pause before deleting
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+  }
+
+  setTimeout(typeEffect, isDeleting ? 100 : 150); // Typing speed
+}
+
+typeEffect();
+
